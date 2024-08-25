6b0d760dcf9c8e96f818bf3073898a87a7d476b3
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-08-25 15:52:07 -0600

    UploadimageFromGallery Compose

--- app/src/main/java/com/carlostorres/firestorage/compose/ui/upload/UploadComposeActivity.kt
+++ app/src/main/java/com/carlostorres/firestorage/compose/ui/upload/UploadComposeActivity.kt
@@ -7,11 +7,21 @@ import android.os.Bundle
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,6 +33,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.core.content.FileProvider
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.carlostorres.firestorage.R
@@ -57,19 +68,86 @@ class UploadComposeActivity : AppCompatActivity() {
 
     @Composable
     fun UploadScreen(
-        viewModel : UploadComposeViewModel = hiltViewModel()
+        viewModel: UploadComposeViewModel = hiltViewModel()
     ) {
+
+        var showImageDialog by remember {
+            mutableStateOf(false)
+        }
 
         var uri: Uri? by remember {
             mutableStateOf(null)
         }
 
-        var intentCameraLauncher =
+        val intentCameraLauncher =
             rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { result ->
                 if (result && uri?.path?.isNotBlank() == true) {
                     viewModel.uploadBasicImage(uri!!)
                 }
             }
+
+        val intentGalleryLauncher =
+            rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { result ->
+                if (result?.path?.isNotBlank() == true) {
+                    viewModel.uploadBasicImage(result)
+                }
+            }
+
+        if (showImageDialog) {
+            Dialog(
+                onDismissRequest = {
+                    showImageDialog = false
+                }
+            ) {
+                Card(
+                    shape = RoundedCornerShape(12),
+                    elevation = 12.dp
+                ) {
+                    Column(
+                        modifier = Modifier.padding(24.dp)
+                    ) {
+                        OutlinedButton(
+                            onClick = {
+                                uri = generateUri()
+                                intentCameraLauncher.launch(uri!!)
+                                showImageDialog = false
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 8.dp)
+                                .align(Alignment.CenterHorizontally),
+                            border = BorderStroke(2.dp, colorResource(id = R.color.green)),
+                            shape = RoundedCornerShape(40)
+                        ) {
+                            Text(
+                                text = "Camera",
+                                color = colorResource(id = R.color.green)
+                            )
+                        }
+                        
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        OutlinedButton(
+                            onClick = {
+                                intentGalleryLauncher.launch("image/*")
+                                showImageDialog = false
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 8.dp)
+                                .align(Alignment.CenterHorizontally),
+                            border = BorderStroke(2.dp, colorResource(id = R.color.green)),
+                            shape = RoundedCornerShape(40)
+                        ) {
+                            Text(
+                                text = "Gallery",
+                                color = colorResource(id = R.color.green )
+                            )
+                        }
+                    }
+                }
+            }
+        }
 
         Box(
             modifier = Modifier
@@ -79,8 +157,7 @@ class UploadComposeActivity : AppCompatActivity() {
 
             FloatingActionButton(
                 onClick = {
-                    uri = generateUri()
-                    intentCameraLauncher.launch(uri!!)
+                    showImageDialog = true
                 },
                 backgroundColor = colorResource(id = R.color.green),
                 shape = RoundedCornerShape(16.dp)
@@ -98,7 +175,7 @@ class UploadComposeActivity : AppCompatActivity() {
 
     }
 
-    private fun generateUri(): Uri{
+    private fun generateUri(): Uri {
         return FileProvider.getUriForFile(
             Objects.requireNonNull(this),
             "com.carlostorres.firestorage.provider",
@@ -106,8 +183,8 @@ class UploadComposeActivity : AppCompatActivity() {
         )
     }
 
-    private fun createFile() : File {
-        val name : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) + "image"
+    private fun createFile(): File {
+        val name: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) + "image"
         return File.createTempFile(name, ".jpg", externalCacheDir)
     }
 
