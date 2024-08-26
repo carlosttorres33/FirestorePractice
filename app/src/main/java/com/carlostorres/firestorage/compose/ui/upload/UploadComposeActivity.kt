89ac183dd3929a538e201bff23da11aeab9896c3
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-08-25 22:07:52 -0600

    EditName Compose

--- app/src/main/java/com/carlostorres/firestorage/compose/ui/upload/UploadComposeActivity.kt
+++ app/src/main/java/com/carlostorres/firestorage/compose/ui/upload/UploadComposeActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -17,13 +18,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -32,16 +35,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.core.content.FileProvider
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.carlostorres.firestorage.R
 import com.carlostorres.firestorage.compose.presentation.UploadComposeViewModel
 import com.carlostorres.firestorage.databinding.ActivityUploadComposeBinding
@@ -91,10 +98,21 @@ class UploadComposeActivity : AppCompatActivity() {
             mutableStateOf(null)
         }
 
+        var userTitle : String by remember {
+            mutableStateOf("")
+        }
+
+        val focusRequester = remember {
+            FocusRequester()
+        }
+        val focusManager = LocalFocusManager.current
+
         val intentCameraLauncher =
             rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { result ->
                 if (result && uri?.path?.isNotBlank() == true) {
                     viewModel.uploadAndGetImage(uri!!) { downloadUri ->
+                        userTitle = ""
+                        focusManager.clearFocus()
                         resultUri = downloadUri
                     }
                 }
@@ -124,7 +142,7 @@ class UploadComposeActivity : AppCompatActivity() {
                     ) {
                         OutlinedButton(
                             onClick = {
-                                uri = generateUri()
+                                uri = generateUri(userTitle)
                                 intentCameraLauncher.launch(uri!!)
                                 showImageDialog = false
                             },
@@ -219,6 +237,35 @@ class UploadComposeActivity : AppCompatActivity() {
 
             Spacer(modifier = Modifier.weight(1f))
 
+            TextField(
+                value = userTitle,
+                onValueChange = {
+                    userTitle = it
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 36.dp)
+                    .border(
+                        2.dp,
+                        colorResource(id = R.color.green),
+                        RoundedCornerShape(22)
+                    )
+                    .focusRequester(focusRequester),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedLabelColor = Color.Transparent
+                ),
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                )
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
             FloatingActionButton(
                 onClick = {
                     showImageDialog = true
@@ -241,16 +288,18 @@ class UploadComposeActivity : AppCompatActivity() {
 
     }
 
-    private fun generateUri(): Uri {
+    private fun generateUri(userTitle: String): Uri {
         return FileProvider.getUriForFile(
             Objects.requireNonNull(this),
             "com.carlostorres.firestorage.provider",
-            createFile()
+            createFile(userTitle)
         )
     }
 
-    private fun createFile(): File {
-        val name: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) + "image"
+    private fun createFile(userTitle: String): File {
+        val name: String = userTitle.ifEmpty {
+            SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) + "image"
+        }
         return File.createTempFile(name, ".jpg", externalCacheDir)
     }
 
